b7bd2710053bfeffb0a6af566ebcc8d718176579
Alex Hinds <alex.hinds141@gmail.com>
2018-01-29 11:45:13 +1100

    added logo scaling correctly

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,8 @@
 <template lang="html">
 <figure>
    <svg class="logo" width="100" height="100" viewBox="0 0 100 100"
-       xmlns="http://www.w3.org/2000/svg" :transform="`scale(${scalefactor || 1})`">
+       xmlns="http://www.w3.org/2000/svg"
+       :style="transformObj">
      <g :fill="fill" :stroke="stroke">
        <rect x="0" y="0" width="100" height="100"></rect>
        <text x="25" y="50" text-anchor="middle" alignment-baseline="central" scale="2">A</text>
@@ -15,7 +16,18 @@
 
 <script>
 export default {
-   props: ['stroke', 'fill', 'scalefactor']
+   props: ['stroke', 'fill', 'scalefactor'],
+   computed: {
+      transformObj() {
+         return {
+            transform: `scale(${this.scalefactor || 1})`,
+            '-ms-transform': `'scale(${this.scalefactor || 1})'`,
+            '-webkit-transform': `scale(${this.scalefactor || 1})`,
+            '-moz-transform': `'scale(${this.scalefactor || 1})'`,
+            '-o-transform': `'scale(${this.scalefactor || 1})'`
+         }
+      }
+   }
 }
 </script>
 
