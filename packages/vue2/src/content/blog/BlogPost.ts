42ccbe1b02cdea678de39a35704d7f24c1bc80c3
Alex Hinds <ahinds@atlassian.com>
2022-12-29 21:39:16 +1100

    chore: add github actions amend

--- packages/vue2/src/content/blog/BlogPost.ts
+++ packages/vue2/src/content/blog/BlogPost.ts
@@ -3,6 +3,7 @@ export default {
     return {
       path: `/post/${id}.json`,
       resolve: (response, mappers) => {
+        // eslint-disable-next-line prefer-const
         let { title, content, meta } = response //.results[0]
         content = Buffer.from(content, 'base64').toString('ascii')
         return mappers.merge({ title, content, ...meta })
