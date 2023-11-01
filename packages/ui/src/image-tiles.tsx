ffecceb2ad121b9c3bc8461ce04239b6c1023633
Alex Hinds <alex.hinds141@gmail.com>
2023-11-01 15:11:06 +1100

    chore: update static asset path

--- packages/ui/src/image-tiles.tsx
+++ packages/ui/src/image-tiles.tsx
@@ -1,26 +1,50 @@
-export const ImageTiles = () => {
+export const ImageTiles = ({
+  staticAssetsUrl = 'https://static.alhinds.com',
+}) => {
   return (
-    <div className="columns-2 gap-4">
-      <img
-        className="object-cover mb-4"
-        src="https://static.alhinds.com/img/cycling.jpg"
-      />
-      <img
-        className="object-cover mb-4"
-        src="https://static.alhinds.com/img/profile_alt.jpg"
-      />
-      <img
-        className="object-cover mb-4"
-        src="https://static.alhinds.com/img/teaching.jpg"
-      />
-      <img
-        className="object-cover mb-4"
-        src="https://static.alhinds.com/img/podcast.jpg"
-      />
-      <img
-        className="object-cover mb-4"
-        src="https://static.alhinds.com/img/profile_landscape.jpg"
-      />
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full object-right"
+          alt="Alex interviews Darren Lapthorne in his time as a journalist."
+          src={`${staticAssetsUrl}/img/cycling.jpg`}
+        />
+      </div>
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full"
+          alt="Alex at the Tour de France."
+          src={`${staticAssetsUrl}/img/profile_landscape.jpg`}
+        />
+      </div>
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full object-left"
+          alt="Alex at a playground. A regular spot of any dad with young kids."
+          src={`${staticAssetsUrl}/img/profile_alt.jpg`}
+        />
+      </div>
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full object-left"
+          alt="Alex teaches the concept of abstract data types to a class of students."
+          src={`${staticAssetsUrl}/img/teaching.jpg`}
+        />
+      </div>
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full"
+          alt="Alex hosting a podcast at SBS."
+          src={`${staticAssetsUrl}/img/podcast.jpg`}
+        />
+      </div>
+      <div className="h-48">
+        <img
+          className="object-cover w-full h-full"
+          alt="Alex working with others at Atlassian."
+          src="http://localhost:3030/img/working.jpg"
+        />
+      </div>
     </div>
   )
 }
