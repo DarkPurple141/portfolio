4f32964153e4a8478853926f1cf886cb213350eb
Alex Hinds <alex.hinds141@gmail.com>
2023-11-19 16:43:58 +1100

    chore: update og image

--- apps/next/app/posts/[slug]/opengraph-image.tsx
+++ apps/next/app/posts/[slug]/opengraph-image.tsx
@@ -57,23 +57,23 @@ export default async ({ params }: Params) => {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-start justify-between bg-slate-1000 px-16 py-8">
+      <div tw="flex flex-col w-full h-full items-start justify-between bg-slate-950 px-16 py-8">
         <div tw="flex flex-col text-slate-100">
-          <p tw="m-0 leading-none text-[#98c0d7]">Read article</p>
-          <h1 tw="mt-2 text-4xl font-bold tracking-tight">
+          <p tw="m-0 text-xl leading-none text-[#98c0d7]">Read article</p>
+          <h1 tw="mt-2 text-5xl font-bold tracking-tight">
             {post.frontmatter.title}
           </h1>
         </div>
         <div tw="flex text-slate-300">
           <img
-            tw="bg-[#98c0d7] w-13 h-13 mr-4"
+            tw="bg-[#98c0d7] w-16 h-16 mr-4 rounded-full"
             src="https://github.com/darkpurple141.png"
             width="100"
             height="100"
           />
-          <div tw="flex flex-col">
+          <div tw="flex flex-col text-xl">
             <h2 tw="m-0">Alex Hinds</h2>
-            <p tw="m-0 text-lg">
+            <p tw="m-0">
               {`https://alhinds.com/posts/${post.frontmatter.slug}`}
             </p>
           </div>
@@ -83,6 +83,8 @@ export default async ({ params }: Params) => {
     {
       // @ts-expect-error
       fonts: await getFonts(),
+      width: 1200,
+      height: 630,
     }
   )
 }
