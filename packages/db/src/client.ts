adf9baee680a6a39039b4b633c8008b057453187
Alex Hinds <alex.hinds141@gmail.com>
2026-01-01 21:21:17 +1100

    chore: add dotenv dependency, update RSS feed timestamps, and modify TypeScript configuration

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from '../prisma/generated/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
+const url = new URL('../prisma/dev.db', import.meta.url)
+
+const adapter = new PrismaBetterSqlite3({
+  url: url.toString(),
+})
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma || new PrismaClient()
+export const prisma = global.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
